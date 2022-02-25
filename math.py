a08b3e347eb54b2469641138d10506fb99c2aeab
madhurisandhi <100432396+madhurisandhi@users.noreply.github.com>
2022-02-25 22:53:49 +0530

    Initial commit of math.py file.

---
+++ math.py
@@ -0,0 +1,22 @@
+#Add implementation
+def add(x,y):
+	return x+y
+    
+#subtract implementation
+def subtract(x,y):
+	return x-y              # on master branch
+    
+#multiply implementation
+def multiply(x,y):
+	return x*y                  # on bug branch
+    
+#divide implementation
+def divide(x,y):
+	if y==0:        # on master branch
+       return DIVIDE_BY_0_ERROR
+    else:
+        return x/y
+
+# square implementation
+def square(x):
+    return x*x
